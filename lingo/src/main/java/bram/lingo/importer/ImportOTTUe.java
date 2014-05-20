f613bcb77ef9a11429922d98adbd1f906cfad563
Bram de Beer <bram.de.beer@stoneroos.nl>
2014-05-20 17:18:51 +0200

    Added Import OTTUE

---
+++ lingo/src/main/java/bram/lingo/importer/ImportOTTUe.java
@@ -0,0 +1,86 @@
+package bram.lingo.importer;
+
+import java.io.File;
+
+import bram.lingo.exporter.WordSetToFileExporter;
+import bram.lingo.words.Word;
+import bram.lingo.words.wordSets.OrderedWordSet;
+import bram.lingo.words.wordSets.WordSet;
+
+public class ImportOTTUe {
+
+	private static final String EXPORT_LOCATION = "src/main/resources/wordlists/ottue/";
+	private static final String OPENTAAL_LIST_LOCATION = "src/main/resources/wordlists/opentaal/OpenTaal-210G-basis-gekeurd.txt";
+	private static final String TUE_LIST_LOCATION = "src/main/resources/wordlists/tue/woorden.med";
+	private static final int SHORTEST_WORD = 5;
+	private static final int LONGEST_WORD = 8;
+	
+	
+	public static void main(String[] args) {
+		WordSet tueWordSet = readWordSet(TUE_LIST_LOCATION);
+		WordSet filteredTueSet = filterWordSet(tueWordSet);
+		WordSet otWordSet = readWordSet(OPENTAAL_LIST_LOCATION);
+		WordSet filteredOtSet = filterWordSet(otWordSet);
+		for (int wordLength = SHORTEST_WORD ; wordLength < LONGEST_WORD + 1 ; wordLength++) {
+			WordSet tueWordsOfLength = filterWordLength(filteredTueSet, wordLength);
+			WordSet otWordsOfLength = filterWordLength(filteredOtSet, wordLength);
+			WordSet wordsOfLength = mergeWordSets(tueWordsOfLength, otWordsOfLength);
+			String exportFilename = Integer.toString(wordLength) + ".txt";
+			exportWordList(wordsOfLength, exportFilename);
+		}
+	}
+
+	
+	private static WordSet mergeWordSets(WordSet wordSet1,
+			WordSet wordSet2) {
+		WordSet mergedSet = new OrderedWordSet();
+		for (Word word : wordSet1) {
+			if (!mergedSet.contains(word)) {
+				mergedSet.addWord(word);
+			}
+		}
+		for (Word word : wordSet2) {
+			if (!mergedSet.contains(word)) {
+				mergedSet.addWord(word);
+			}
+		}
+		return mergedSet;
+	}
+
+
+	private static void exportWordList(WordSet wordSet,
+			String exportFilename) {
+		File exportMap = new File(EXPORT_LOCATION);
+		WordSetToFileExporter exporter = new WordSetToFileExporter(wordSet, exportMap, exportFilename);
+		exporter.write();
+	}
+
+
+	private static WordSet filterWordLength(WordSet totalSet, int wordLength) {
+		WordSet wordSetOfLength = new WordSet();
+		for (Word word : totalSet) {
+			if (word.length() == wordLength) {
+				wordSetOfLength.addWord(word);
+			}
+		}
+		return wordSetOfLength;
+	}
+
+
+	private static WordSet filterWordSet(WordSet unfilteredSet) {
+		WordSet filteredSet = new WordSet();
+		for (Word word : unfilteredSet) {
+			if (word.isCorrectLingoWord()) {
+				filteredSet.addWord(word);
+			}
+		}
+		return filteredSet;
+	}
+
+	private static WordSet readWordSet(String location) {
+		File file = new File(location);
+		FileToWordSetImporter importer = new FileToWordSetImporter(file);
+		return importer.read();
+	}
+	
+}
