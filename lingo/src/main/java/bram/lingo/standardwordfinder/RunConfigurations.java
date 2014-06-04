812dcff38707d22318fcc5a1219d41838fdc35a8
Bram de Beer <bram.de.beer@stoneroos.nl>
2014-06-04 17:21:48 +0200

    Changed Run Config

--- lingo/src/main/java/bram/lingo/standardwordfinder/RunConfigurations.java
+++ lingo/src/main/java/bram/lingo/standardwordfinder/RunConfigurations.java
@@ -15,14 +15,14 @@ public class RunConfigurations {
 	
 	public final String fileLocation = "src/main/resources/result/";
 	public final String runningPrefix = "running_";
-	public final String descriptionPrefix = "G_Algorithm_Genetic_";
+	public final String descriptionPrefix = "E_Algorithm_Genetic_";
 	public final int wordLength = 7;
 	public final Source source = Source.OTTUE;
 	public final boolean printToFile = true;
 	public final boolean appendTimestampToFilename = true;
 	public final boolean printTime = true;
 	public final int minSubsetSize = 1;
-	public final int maxSubsetSize = 3;
+	public final int maxSubsetSize = 2;
 	public final Select select = Select.BEST;
 	public final boolean runAllLetters = true;
 	public final Letter[] lettersToRun = {Letter.ij, Letter.j, Letter.q, Letter.x, Letter.y};
@@ -50,9 +50,9 @@ public class RunConfigurations {
 		algorithmMap.put(Algorithm.C1, RunType.None);
 		algorithmMap.put(Algorithm.C2, RunType.None);
 		algorithmMap.put(Algorithm.D2, RunType.None);
-		algorithmMap.put(Algorithm.E1, RunType.None);
+		algorithmMap.put(Algorithm.E1, RunType.Exhaustive);
 		algorithmMap.put(Algorithm.F1, RunType.None);
-		algorithmMap.put(Algorithm.G1, RunType.Genetic1M);
+		algorithmMap.put(Algorithm.G1, RunType.None);
 		algorithmMap.put(Algorithm.H1, RunType.None);
 		algorithmMap.put(Algorithm.I1, RunType.None);
 		algorithmMap.put(Algorithm.J2, RunType.None);
